e48a5d90ca15af188467cd7b25d79f543109b8df
白石拓 <shiraishitaku@shiraishitakunoMacBook-Pro.local>
2022-12-25 23:36:21 +0900

    ruby

--- index2.rb
+++ index2.rb
@@ -1,5 +1,34 @@
 language = ["日本語","英語", "スペイン語"]
+border = "---------------------"
 
 language.each do |language|
+  puts border
   puts "#{language}を話せます"
 end
+
+exam = {"subject" => "Math","score" => 80}
+
+puts exam ["subject"]
+
+exam ["subject"] = "science"
+
+puts exam ["subject"]
+
+exam ["grade"] ="good"
+
+puts exam["grade"]
+
+exam = {:subject => "Math", :score => 80}
+
+exam = {:subject => "Math" , :score => 80}
+puts exam[:score]
+
+exam = {subject:"Math",score:80}
+
+puts "#{exam[:subject]}: #{exam[:score]}点"
+
+exam = {subject: "Math" ,score: 80}
+
+puts exam [:grade]
+
+puts nil
