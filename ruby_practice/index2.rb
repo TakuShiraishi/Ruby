903ddb012f81ab3e5b90907d0fe8f0b83bcf9c8b
白石拓 <shiraishitaku@shiraishitakunoMacBook-Pro.local>
2023-01-01 19:28:13 +0900

    aaa

--- ruby_practice/index2.rb
+++ ruby_practice/index2.rb
@@ -3,3 +3,9 @@ language = ["日本語","英語", "スペイン語"]
 language.each do |language|
   puts "#{language}を話せます"
 end
+
+language = ["日本語","英語", "スペイン語"]
+
+language.each do |language|
+  puts "#{language}を話せます"
+end
